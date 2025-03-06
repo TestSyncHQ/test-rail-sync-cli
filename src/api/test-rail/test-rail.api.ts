b129f3bcf5400bacc6a7adf8dd6c9bf38c953fb2
jmejiadev <jmejiaxdev@q4inc.com>
2025-03-06 16:20:15 -0500

    #2

--- src/api/test-rail/test-rail.api.ts
+++ src/api/test-rail/test-rail.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Case } from "../../definitions/case.definitions";
 import { CONFIG } from "../../definitions/config.definitions";
 
-const { organizationUrl, username, password } = CONFIG.api;
+const { organizationUrl, username, password, projectId, suiteId } = CONFIG.api;
 
 const axiosInstance = axios.create({
   baseURL: `${organizationUrl}/api/v2/`,
@@ -14,4 +14,14 @@ const axiosInstance = axios.create({
 export async function addCase(testCase: Case): Promise<Case> {
   const response = await axiosInstance.post(`add_case/${testCase.section_id}`, testCase);
   return response.data;
+}
+
+export async function getCase(id: Case["id"]): Promise<Case> {
+  const response = await axiosInstance.get(`get_test/${id}`);
+  return response.data;
+}
+
+export async function getCases(sectionId: Case["section_id"]): Promise<Case[]> {
+  const response = await axiosInstance.get(`get_cases/${projectId}&suite_id=${suiteId}&section_id=${sectionId}`);
+  return response.data.cases;
 }
